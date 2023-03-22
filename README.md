4b9dacb430f8e43cd837fa97639a441411088d73
Andeilso Alves <andeilsoalves@gmail.com>
2023-03-22 10:47:16 -0300

    Desafio TIMERv1.2 concluido. Alterações no README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# curso_javascript_e_typescript
-Desafios do curso "Curso de JavaScript e TypeScript do básico ao avançado" realizados.
+<h1 align="center"> Curso JavaScript e TypeScript do Básico ao avançado </h1>
+
+## Desafios realizados
+- TIMER
+
+
+## Tecnologias
+- HTML e CSS
+- Javascript
+
+
+[Curso de JavaScript e TypeScript do básico ao avançado](https://www.udemy.com/course/curso-de-javascript-moderno-do-basico-ao-avancado/) by Luiz Otávio Miranda
+
---
+++ timer/main.js
@@ -0,0 +1,73 @@
+//Criação de variáveis
+let sec = 0;
+let min = 0;
+let hour = 0;
+let count;
+let init = false;
+
+//Elementos do HTML
+let pTimer = document.getElementById("timer");
+
+document.addEventListener('click', (e) => {
+    if(e.target.id === 'start'){
+        pTimer.style = "color: white;"
+
+        if(!init){
+            count = setInterval( () => {
+                sec++
+            
+                if(sec == 60){
+                    min++
+                    sec = 0;
+                }
+            
+                if(min == 60){
+                    hour++
+                    min = 0;
+                }
+            
+                pTimer.innerHTML = `${addZeros(hour)}:${addZeros(min)}:${addZeros(sec)}`;
+            }, 1000);
+        } else {
+            return
+        }
+    
+        return init = true;
+    }
+    
+    if(e.target.id === 'stop'){
+        clearInterval(count);
+    
+        if(init){
+            pTimer.style = "color: rgb(51, 170, 91);"
+        }
+    
+        return init = false; 
+    }
+    
+    if(e.target.id === 'reset'){
+        clearInterval(count);
+    
+        sec = 0;
+        min = 0;
+        hour = 0;
+        
+        pTimer.style = "color: white;"
+        
+        pTimer.innerHTML = `${addZeros(hour)}:${addZeros(min)}:${addZeros(sec)}`;
+
+        return init = false;
+    }
+})
+
+//Funções
+let addZeros = (time) => {
+    time = time.toString();
+
+    if(time.length < 2){
+        time = "0" + time;
+        return time;
+    }
+
+    return time;
+};
