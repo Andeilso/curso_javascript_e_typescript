378720e519d22d70b8eba0abb10e0266f1fa92f1
Andeilso Alves <andeilsoalves@gmail.com>
2023-04-05 07:06:33 -0300

    Mudança visual no input

--- desafio_lista_de_tarefas/main.js
+++ desafio_lista_de_tarefas/main.js
@@ -2,27 +2,50 @@ let tarefaInput = document.getElementById('tarefa');
 let lista = document.getElementById('lista-ul');
 listaIndex = lista.children.length;
 
-document.addEventListener('click', (e) => {
-    if (e.target.value === 'adicionar') {
-
-        if(tarefaInput.value == ''){
-            return
-        }
-
-        lista.appendChild(document.createElement('li'));
-        lista.children[listaIndex].innerHTML = `${tarefaInput.value} <button id="excluir">Excluir</button>`;
-        listaIndex++;
+let adicionarTarefa = () => {
+    if(tarefaInput.value == ''){
+        return
     }
 
-    if(e.target.id === 'excluir'){
-        for (let li = 0; li < lista.children.length; li++) {
-            if(e.target.parentNode.innerHTML === lista.children[li].innerHTML){
-                lista.removeChild(lista.children[li]);
+    lista.appendChild(document.createElement('li'));
+    lista.children[listaIndex].innerHTML = `${tarefaInput.value} <button id="excluir">Excluir</button>`;
+    listaIndex++;
+    console.log(lista)
+};
 
-                return
-            };
-        }
+let excluirTarefa = (e) => {
+    for (let li = 0; li < lista.children.length; li++) {
+        if(e.target.parentNode.innerHTML === lista.children[li].innerHTML){
+            listaIndex--;
+            return lista.removeChild(lista.children[li]);
+        };
+    }
+};
+
+let limparCampoDeTarefa = () => {
+    tarefaInput.value = '';
+    tarefaInput.focus();
+}
+
+document.addEventListener('keypress', (e) => {
+    if(e.keyCode === 13){
+        adicionarTarefa();
+        limparCampoDeTarefa();
     }
 })
 
+document.addEventListener('click', (e) => {
+    if (e.target.id === 'adicionar') {
+        adicionarTarefa(); 
+        limparCampoDeTarefa();
+    }
+
+    if(e.target.id === 'excluir'){
+        excluirTarefa(e);
+    }
+
+    console.log(e.target);
+})
+
+tarefaInput.focus();
 
